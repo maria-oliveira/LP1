f2a9a18668b718ec758fd39064e24d31713d2bf6
Maria Eduarda <157076458+maria-oliveira@users.noreply.github.com>
2025-10-16 12:31:37 -0300

    Delete Dorival Caymmi directory

--- Dorival Caymmi/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mariaeduarda.dorivalcaymmi {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.mariaeduarda.dorivalcaymmi to javafx.fxml;
-    exports com.mariaeduarda.dorivalcaymmi;
-}
